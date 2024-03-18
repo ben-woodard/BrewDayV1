1ceaa0f8838a9e4ee8f96ac39ae9855d11421876
Ben Woodard <143208459+ben-woodard@users.noreply.github.com>
2024-03-17 20:32:37 -0400

    registration.html and enpoints working
    added dashboard.html

--- src/main/java/com/jobtrackingapp/brewday/service/CompanyService.java
+++ src/main/java/com/jobtrackingapp/brewday/service/CompanyService.java
@@ -1,7 +1,25 @@
 package com.jobtrackingapp.brewday.service;
 
+import com.jobtrackingapp.brewday.domain.Company;
+import com.jobtrackingapp.brewday.repository.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CompanyService {
+
+    private final CompanyRepository companyRepo;
+
+    @Autowired
+    public CompanyService(CompanyRepository companyRepo) {
+        this.companyRepo = companyRepo;
+    }
+
+    public Company save(Company company) {
+        return companyRepo.save(company);
+    }
+
+    public Company findById(Long companyId) {
+        return companyRepo.findById(companyId).orElse(null);
+    }
 }
--- src/main/java/com/jobtrackingapp/brewday/web/CompanyController.java
+++ src/main/java/com/jobtrackingapp/brewday/web/CompanyController.java
@@ -1,16 +1,50 @@
 package com.jobtrackingapp.brewday.web;
 
 import com.jobtrackingapp.brewday.domain.Company;
+import com.jobtrackingapp.brewday.service.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CompanyController {
 
-    @GetMapping(value = {"", "/", "/register"})
+    private final CompanyService companyService;
+
+    @Autowired
+    public CompanyController(CompanyService companyService) {
+        this.companyService = companyService;
+    }
+
+    @GetMapping(value = {"", "/"})
+    public String redirectToRegister() {
+        return "redirect:/register";
+    }
+
+
+    @GetMapping("/register")
     public String getRegister (ModelMap model) {
         model.put("company", new Company());
         return "register";
+    }
+
+    @PostMapping("/register")
+    public String postRegister(Company company) {
+        companyService.save(company);
+        Long companyId = company.getCompanyId();
+        return "redirect:/dashboard/" + companyId;
+    }
+
+    @GetMapping("/dashboard/{companyId}")
+    public String getCompanyDashboard(ModelMap model, @PathVariable Long companyId) {
+        Company savedCompany = companyService.findById(companyId);
+        if(savedCompany == null) {
+            return "redirect:/register";
+        }
+        model.put("company", savedCompany);
+        return "dashboard";
     }
 }
